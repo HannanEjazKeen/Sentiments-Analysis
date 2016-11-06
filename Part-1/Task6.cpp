7e9ffa5f284c93a308947bc00c43c18d8aabe679
Hannan Ejaz Keen <keenhannan@gmail.com>
2016-11-06 11:15:48 +0500

    Create Task6.cpp

---
+++ Part-1/Task6.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	char in[20] = {'\0'};
+	cout<<"Enter First Name"<<endl;
+	cin.getline(in,20);
+
+	ofstream write;
+	write.open("data.txt",ios::out);
+	if (!write.is_open())
+	{
+		cout<<"File not opened"<<endl;
+		return 0;
+	}
+	write<<in<<endl;
+	write.close();
+
+	char in2[20] = {'\0'};
+	ifstream read;
+	read.open("data.txt",ios::in);
+	if (!read.is_open())
+	{
+		cout<<"File not opened"<<endl;
+		return 0;
+	}
+	read.getline(in2,20);
+	read.close();
+	cout<<in2<<endl;
+}
